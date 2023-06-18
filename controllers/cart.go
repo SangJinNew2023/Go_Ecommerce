0dbb596d242a721e0dce245459c52288ec5825bb
Sang jin An <photoan79@naver.com>
2023-06-18 01:20:55 -0700

    add cart

--- controllers/cart.go
+++ controllers/cart.go
@@ -3,12 +3,14 @@ package controllers
 import (
 	"context"
 	"ecommerce/database"
+	"ecommerce/models"
 	"errors"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -119,12 +121,60 @@ func (app *Application) RemoveItem() gin.HandlerFunc {
 }
 
 func (app *Application) GetItemFromCart() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		//Query returns the keyed url query value if it exists, otherwise it returns an empty string `("")`.
+		user_id := c.Query("id")
+		if user_id == "" {
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H{"error": "invalid id"})
+			c.Abort()
+			return
+		}
 
+		//bjectIDFromHex creates a new ObjectID from a hex string. It returns an error if the hex string is not a valid ObjectID.
+		usert_id := primitive.ObjectIDFromHex(user_id)
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var filledcart models.User
+		//FindOne()은 bson.D{}에 해당하는 데이터를 Usercollection으로 부터 찾고 decode후 &founduser에 저장
+		err := UserCollection.FindOne(ctx, bson.D{primitive.E{Key: "_id", Value: usert_id}}).Decode(&filledcart)
+
+		if err != nil {
+			log.Println(err)
+			c.IndentedJSON(500, "not found")
+			return
+		}
+
+		filter_match := bson.D{{Key: "$match", Value: bson.D{primitive.E{Key: "_id", Value: usert_id}}}}
+		unwind := bson.D{{Key: "$unwind", Value: bson.D{primitive.E{Key: "path", Value: "$usercart"}}}}
+		grouping := bson.D{{Key: "$group", Value: bson.D{primitive.E{Key: "_id", Value: "$_id"},
+			{Key: "total", Value: bson.D{primitive.E{Key: "$sum", Value: "$usercart.price"}}}}}}
+
+		pointcursor, err := UserCollection.Aggregate(ctx, mongo.Pipeline{filter_match, unwind, grouping})
+		if err != nil {
+			log.Println(err)
+		}
+
+		var listing []bson.M
+		if err = pointcursor.All(ctx, &listing); err != nil {
+			log.Println(err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+		}
+
+		for _, json := range listing {
+			c.IndentedJSON(200, json["total"])
+			c.IndentedJSON(200, filledcart.UserCart)
+		}
+		ctx.Done()
+
+	}
 }
 
 func (app *Application) BuyFromCart() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		userQueryId := c.Query("id")
+		userQueryID := c.Query("id")
 
 		if userQueryID == "" {
 			log.Panicln("user id is empty")
@@ -132,7 +182,6 @@ func (app *Application) BuyFromCart() gin.HandlerFunc {
 		}
 
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
-
 		defer cancel()
 
 		err := database.BuyItemFromCart(ctx, app.userCollection, userQueryID)
@@ -160,9 +209,9 @@ func (app *Application) InstantBuy() gin.HandlerFunc {
 			_ = c.AbortWithError(http.StatusBadRequest, errors.New("product id is empty"))
 			return
 		}
+
 		//bjectIDFromHex creates a new ObjectID from a hex string. It returns an error if the hex string is not a valid ObjectID.
 		productID, err := primitive.ObjectIDFromHex(productQueryID)
-
 		if err != nil {
 			log.Println(err)
 			c.AbortWithStatus(http.StatusInternalServerError)
@@ -170,11 +219,9 @@ func (app *Application) InstantBuy() gin.HandlerFunc {
 		}
 
 		var ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
-
 		defer cancel()
 
 		err = database.InstantBuyer(ctx, app.prodCollection, app.userCollection, productID, userQueryID)
-
 		//IndentedJSON serializes the given struct as pretty JSON (indented + endlines) into the response body
 		if err != nil {
 			c.IndentedJSON(http.StatusInternalServerError, err)
