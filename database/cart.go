76391f6c6e52e0896140d5ed79e1409405f3a385
Sang jin An <photoan79@naver.com>
2023-06-23 16:17:43 -0700

    part22

--- database/cart.go
+++ database/cart.go
@@ -46,11 +46,49 @@ func AddProductToCart(ctx context.Context, prodCollection userCollection *mongo.
 	_, err = userCollection.UpdateOne(ctx, filter, update)
 }
 
-func RemoveCartItem() {
+func RemoveCartItem(ctx context.Context, prodcollection, userCollection *mongo.Collection, productID primitive.ObjectID, userID string) error {
+	id, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		log.Println(err)
+		return ErrUserIdIsNotValid
+	}
+	filter := bson.D(primitive.E{Key:"_id", Value:id})
+	update := bson.M{"$pull":bson.M{"usercart": bson.M{"_id":productID}}}
+	_, err = UpdateMany(ctx, filter, update)
+	if err != nil {
+		return ErrCantRemoveItemCart
+	}
+	return nil
 
 }
 
-func BuyitemFromCart() {
+func BuyItemFromCart(ctx context.Context, userCollection *mongo.Collection, userID string) error {
+	//fetch the cart of the user
+	//find the cart total
+	//create an order with the items
+	//empty up the cart
+
+	primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		log.Println(err)
+		return ErrUserIdIsNotValid
+	}
+	var getcartitems models.User
+	var ordercart models.Order
+
+	ordercart.Order_ID = primitive.NewObjectID()
+	ordercart.Ordered_At = time.Now()
+	ordercart.Order_Cart = make([]models.ProductUser, 0)
+	ordercart.Payment_Method.COD = true
+
+	unwind := bson.D{{Key:"$unwind", Value:bson.D{primitive.E{Key:"path", Value:"$usercart"}}}}
+	grouping := bson.D{{Key:"$group", Value:bson.D{primitive.E{Key:"_id", Value:"$_id"}, {Key:"total", Value: bson.D{primitive.E{Key:"$sum", Value:"$usercart.price"}}}}}}
+	currentresults, err := userCollection.Aggregate(ctx, mongo.Pipeline{unwind, grouping})
+	ctx.Done()
+	if err != nil {
+		panic(err)
+	}
+
 
 }
 
